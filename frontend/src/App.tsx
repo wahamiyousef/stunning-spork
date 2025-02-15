84f32e986a4915740fc924e6167f3722c331b871
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2025-02-15 14:22:34 -0500

    initalize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <h1>hello</h1>
+      <Button>button</Button>
+    </div>
+  )
+}
+
+export default App
